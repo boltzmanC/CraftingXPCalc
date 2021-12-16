b3c6272325ea8462fd952c626f625b23f8073d4c
Dylan White <Dylan.White@team.neustar>
2021-12-16 10:54:45 -0800

    Add project files.

---
+++ CraftingXPCalc/StoneCuttingXP.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CraftingXPCalc
+{
+    class StoneCuttingXP
+    {
+
+        const int stonebrick = 36;
+        const int lodestonebrick = 180;
+        const int obsidianvoidstone = 9240;
+
+        public static int GetValueByName(string name)
+        {
+            switch (name)
+            {
+                case "bob": return 1;
+                case "billy": return 2;
+                default: return -1;
+            }
+        }
+
+    }
+}
